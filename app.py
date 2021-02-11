c7e60ed9fa4d510c6cb85065e9f707fa101dd489
sandeeprenduchintala <64719534+sandeeprenduchintala@users.noreply.github.com>
2021-02-11 17:58:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']= 'root'
-app.config['MYSQL_PASSWORD']='Schnrtrt200@'
+app.config['MYSQL_PASSWORD']=''
 app.config['MYSQL_DB']='frappe_assess'
 mysql = MySQL(app)
 
